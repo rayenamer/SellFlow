91f787b86a6d0f12a2bdb0998f21c55e44adee5c
rayenamer <rayen.ameur@esprit.tn>
2025-03-26 18:18:54 +0100

    restart

--- SellFlowApi/API/obj/Debug/net8.0/API.AssemblyInfo.cs
+++ SellFlowApi/API/obj/Debug/net8.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+235d1937e33aadcb802a9a76f6c543663ba8476c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8a3c4af65cc1dc9185be1c2baeafefc93e479818")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
